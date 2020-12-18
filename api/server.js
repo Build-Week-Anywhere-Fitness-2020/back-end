e495782648898e85f0fc4423ff34b0ae781a88e6
Rigo <limon1285@gmail.com>
2020-12-17 22:12:01 -0600

    update

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
-//test
+const express = require("express");
+const router = express.Router();
+const helmet = require("helmet");
+const cors = require("cors");
+
+//server
+const server = express();
+
+//Global middleware
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+//Server endpoints --------->
+
+//global middleware for .catch on all endpoints
+server.use((err, req, res, next) => {
+  console.log("err--->", err);
+  res.status(500).json({ Error: "500 Error, what happened?" });
+});
+
+module.exports = server;
