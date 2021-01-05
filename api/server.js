eda7034fc042ee06b456a6086ac435b2381acb60
Rigo <limon1285@gmail.com>
2021-01-04 18:29:45 -0600

    updated, done, might need to tweak more

--- api/server.js
+++ api/server.js
@@ -1,1 +1,34 @@
-//test
+const express = require("express");
+const router = express.Router();
+const helmet = require("helmet");
+const cors = require("cors");
+const { logger, restrict } = require("../middlewares/middleware-stacks");
+
+//server
+const server = express();
+
+//import routers
+const welcomeRouter = require("../welcome/welcome-router");
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
+const classesRouter = require("../classes/classes-router");
+
+//Global middleware
+server.use(helmet());
+server.use(cors());
+server.use(logger("long"));
+server.use(express.json());
+
+//Server endpoints --------->
+server.use("/", welcomeRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", restrict(), usersRouter);
+server.use("/api/classes", restrict(), classesRouter);
+
+//global middleware for .catch on all endpoints
+server.use((err, req, res, next) => {
+  console.log("err in global catch--->", err);
+  res.status(500).json({ Error: "Error, something wrong with the server" });
+});
+
+module.exports = server;
